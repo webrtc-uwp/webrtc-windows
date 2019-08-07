fe229a22804f985b948bcd301aa488adf1c6bb08
Jerome Humbert <jerome.humbert@microsoft.com>
2019-08-07 12:40:26 +0200

    Add extra headers to pch.h
    Forward-port changes of #61 from releases/m71 to releases/m75.

--- projects/msvc/Org.WebRtc.Universal/pch.h
+++ projects/msvc/Org.WebRtc.Universal/pch.h
@@ -8,8 +8,11 @@
 #include "cppwinrt_Helpers.h"
 
 #include <unknwn.h>
+#include <winrt/Windows.Devices.Enumeration.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Media.Core.h>
+#include <winrt/Windows.UI.Core.h>
 #include <winrt/Windows.UI.Xaml.h>
 #include <winrt/Windows.UI.Xaml.Controls.h>
 #include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
@@ -17,3 +20,5 @@
 #include <winrt/Windows.UI.Xaml.Interop.h>
 #include <winrt/Windows.UI.Xaml.Markup.h>
 #include <winrt/Windows.UI.Xaml.Navigation.h>
+
+#include "winrt/Org.WebRtc.h"
